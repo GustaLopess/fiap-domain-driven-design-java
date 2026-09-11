1fbb5a8dc92822d743f50c54ddbe4b32cd2a69cd
Gustavo Lopes <gustavo.souza0911@gmail.com>
2026-09-10 21:43:53 -0300

    feat: Alterando valores do objeto tv

--- FiapAula/src/aula03/TelevisaoTeste.java
+++ FiapAula/src/aula03/TelevisaoTeste.java
@@ -1,4 +1,23 @@
 package aula03;
 
 public class TelevisaoTeste {
+
+    public static void main(String[] args) {
+        System.out.println("---- Minha Televisão ----");
+
+        // Instanciando Objeto - Tipo = Televisão.
+        Televisao tv = new Televisao();
+
+
+        System.out.println("Canal: " + tv.canal);
+        System.out.println("Volume: " + tv.volume);
+        System.out.println("Ligado: " + tv.ligado);
+
+        // Alterando os dados do objeto tv de forma direta
+
+        tv.canal = 30;
+        tv.volume = 1000;
+        tv.ligado = true;
+
+    }
 }
