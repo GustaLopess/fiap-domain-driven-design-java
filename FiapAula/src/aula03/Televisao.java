6f1c26f1641a7f07af593dc3e604ff494e7647ba
Gustavo Lopes <gustavo.souza0911@gmail.com>
2026-09-10 21:32:43 -0300

    feat: Criando classe televisão

---
+++ FiapAula/src/aula03/Televisao.java
@@ -0,0 +1,14 @@
+package aula03;
+
+public class Televisao {
+
+    // Atributos da classe televisão
+    int canal;
+    int volume;
+    boolean ligada;
+
+//    Metodos da classe televisão
+
+
+
+}
